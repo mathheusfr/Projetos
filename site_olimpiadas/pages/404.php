e880b5befe42cad61b2fc309c01b512267d87060
Matheus Freitas <67061342+mathheusfr@users.noreply.github.com>
2024-10-02 15:24:24 -0300

    Delete site_olimpiadas directory

--- site_olimpiadas/pages/404.php
+++
@@ -1,4 +0,0 @@
-<section class="a404">
-    <h1>Pagina não existe! <hr></h1>
-    <p class="error">Deseja voltar para a <a href="<?php echo INCLUDE_PATH; ?>">Pagina inicial</p>
-</section>
--- site_olimpiadas/painel.php
+++
@@ -1,2 +0,0 @@
-<h1>Ola mundo </h1>
-
