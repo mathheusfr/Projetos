e73015169d20052ebc6157b66190b239d56d248b
Matheus Freitas <67061342+mathheusfr@users.noreply.github.com>
2025-04-02 01:40:44 -0300

    Delete teste.php

--- teste.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-use MercadoPago\SDK;
-
-SDK::setAccessToken("APP_USR-4747529223961987-010813-e7d6f91708b3c8c562cd697f30e43496-2202391298");
-
-echo "Autoloader e Mercado Pago configurados com sucesso!";
